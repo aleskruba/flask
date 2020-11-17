538add8ccaa6fb7c29502f0f8fc78983e1c89cf0
aleskruba <74367962+aleskruba@users.noreply.github.com>
2020-11-17 13:04:13 +0100

    Add files via upload

---
+++ inheritance templates/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    name='pepa'
+    return render_template('Index.html',name=name)
+
+@app.route('/classmates')
+def client():
+    listname=['Jan','Jiri']
+    return render_template('classmates.html',listname=listname)
+
+
+if __name__=='__main__':
+    app.run(port=8000)
